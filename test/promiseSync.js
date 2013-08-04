5c659ac67402cf2ad3bd04d1a89ee21e8c21c13e
Glen Huang <curvedmark@gmail.com>
2013-08-04 13:05:38 +0800

    use mocha-as-promised

--- test/promiseSync.js
+++ test/promiseSync.js
@@ -1,11 +1,13 @@
 var assert = require('assert');
 var Promise = require('../Promise');
+require("mocha-as-promised")();
 
 describe('synchronous promise', function () {
-	it('should accept asynchronous promise', function (done) {
+	it('should accept asynchronous promise', function () {
 		var result = [];
 		var promise = new Promise().fulfill();
-		promise.then(function () {
+
+		return promise.then(function () {
 			var p = new Promise;
 			setTimeout(function () {
 				result.push(1);
@@ -15,20 +17,22 @@ describe('synchronous promise', function () {
 		}).then(function () {
 			result.push(2);
 			assert.deepEqual(result, [1, 2]);
-		}).then(done, done);
+		});
 	});
 
-	it('should allow context to be set', function (done) {
+	it('should allow context to be set', function () {
 		var promise = new Promise().fulfill({}, 1);
-		promise.then(function () {
+
+		return promise.then(function () {
 			assert.equal(this, 1)
-		}).then(done, done);
+		});
 	});
 
-	it('should allow context to be set on child promise', function (done) {
+	it('should allow context to be set on child promise', function () {
 		var promise = new Promise().fulfill({}, 1);
-		promise.then().then(function () {
+
+		return promise.then().then(function () {
 			assert.equal(this, 1);
-		}).then(done, done);
+		});
 	});
 });
